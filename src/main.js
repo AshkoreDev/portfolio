ec44c9b0f62e80c435eebf12b96d7aa104d21571
Aracely Alba @AshkoreDev <aracelyalba8@gmail.com>
2023-01-29 21:41:17 +0100

    Added go up button

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 import './js/skill-card.js';
 import './js/project-card.js';
+
+
+
+ const upBtn = document.getElementById('upBtn');
+
+ upBtn.addEventListener('click', () =>  window.scrollTo(0, 0));
