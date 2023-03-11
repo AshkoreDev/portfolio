bf22c76e87686b87e55e7db2a8924c04ddb85bb6
Aracely Alba <aracelyalba8@gmail.com>
2023-03-10 21:35:38 -0430

    Fixed BtnUp logic

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import './js/project-card.js';
 
 
 
- const upBtn = document.getElementById('upBtn');
+const upBtn = document.getElementById('upBtn');
 
- upBtn.addEventListener('click', () =>  window.scrollTo(0, 0));
+upBtn.addEventListener('click', () =>  window.scrollTo(0, 0));
+
+window.onscroll = () => {
+
+ 	window.scrollY > 1300
+ 		? upBtn.classList.add('upButton-on')
+ 		: upBtn.classList.remove('upButton-on');
+}
